7efff2ce503d77a53b1cb1a74fe92eee6e5f21ae
Yassine <benkhayyassine8@gmail.com>
2022-11-06 09:53:40 -0800

    clean up the routes file

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -7,15 +7,5 @@ const {
   updateGoal,
 } = require("../controllers/goalController");
 router.route("/").get(getGoals).post(setGoal);
-/*
-shortcut of / for the two routes:
-router.get("/", getGoals);
-router.post("/",setGoal);
-*/
 router.route("/:id").put(updateGoal).delete(deleteGoal);
-/*
-the shorcut of /:id of the two routes:
-router.put("/:id", setGoal);
-router.delete("/:id", deleteGoal);
-*/
 module.exports = router;

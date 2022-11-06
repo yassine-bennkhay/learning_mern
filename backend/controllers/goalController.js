ffc08b5934dbb4f23729ef9c109b0d0421b6220c
Yassine <benkhayyassine8@gmail.com>
2022-11-06 13:08:33 -0800

    error handling

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,8 +1,9 @@
+const asyncHandler = require("express-async-handler");
 /* Description      get goals
    Route            GET /api/goals
    Access           private
 */
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   const dummydata = {
     goal_name: "read a bit of design patterns",
     goal_time: "today at night",
@@ -10,31 +11,35 @@ const getGoals = (req, res) => {
   };
   res.status(200).json(dummydata);
   res.end();
-};
+});
 /* Description      add goal
    Route            POST /api/add/goal
    Access           private
 */
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field.");
+  }
   res.status(201).json({ message: `A goal has been added successfully` });
-  res.end()
-};
+  res.end();
+});
 /* Description      update goal
    Route            PUT /api/update/goal
    Access           private
 */
-const updateGoal=(req,res)=>{
-    res.status(200).json({ message: `Updated goal is ${req.params.id}` });
+const updateGoal = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Updated goal is ${req.params.id}` });
   res.end();
-}
+});
 /* Description      delete goal
    Route            DELETE /api/delete/goal
    Access           private
 */
-const deleteGoal=(req,res)=>{
-    res.status(200).json({ message: `Deleted goal is ${req.params.id}` });
-    res.end();
-}
+const deleteGoal = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: `Deleted goal is ${req.params.id}` });
+  res.end();
+});
 module.exports = {
   getGoals,
   setGoal,
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const { use } = require("./routes/goalRoutes");
-//const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleWare/errorMiddleware");
+const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 //endpoints
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/add/goal", require("./routes/goalRoutes"));
 app.use("/api/update/goal", require("./routes/goalRoutes"));
 app.use("/api/delete/goal", require("./routes/goalRoutes"));
+app.use(errorHandler);
 app.listen(port, () => console.log(`Server started on port ${port}`));
